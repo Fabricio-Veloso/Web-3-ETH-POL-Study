3ea7e9a23446da37a00cb660f250361c2c47da7f
FabricioVeloso <fabraccho@gmail.com>
2025-12-29 01:44:43 -0300

    Add simple require use. -> implement events

--- contracts/Couter.sol
+++ contracts/Couter.sol
@@ -1,25 +1,29 @@
 pragma solidity ^0.8.20;
 
-contract counter{
-  uint256 count public;
-  count = 0;
+contract Counter{
+  uint256 public count ;
+  address public owner;
 
-  function increment(uint256 amount){
-    if(msg.sender == owner(this)){
-      count += amount;
-    }
+  constructor(){
+    owner = mensage.sender;
   }
 
-  function decrement(uint256 amount){
-    if (count == 0)  {
-      {
-        return;
-      }
-      else{
-        if (msg.sender == owner(this))) {
-          count -= amount;
-        } 
-      }
+  function  increment (uint256 amount) public{
+    require( msg.sender == owner, "only the owner can increment");
+    require(amount > 0, "amount must be greater than zero");
 
-    }
+    count += amount;
+  }
+
+  function  decrement (uint256 amount) public{
+    require( msg.sender == owner, "only the owner can decrement");
+    require(count >= amount,"Counter shall not go under 0");
+    require(amount > 0, "amount must be greater than zero");
+
+    count -= amount;
+  }
+
+  function reset public(){
+    require(msg.sender == owner, "only the owner can reset");
+    count = 0;
   }
